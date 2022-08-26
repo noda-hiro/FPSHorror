957331120bbf7e00ba8bcf144dd339a494d2c613
aki3 <hiro13424@gmail.com>
2022-08-27 02:45:31 +0900

    feat : プレイヤー移動の追加

--- FPS_HORROR/Assets/Scripts/Player.cs
+++ FPS_HORROR/Assets/Scripts/Player.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UniRx;
+using UniRx.Triggers;
 
 public class Player : MonoBehaviour
 {
@@ -8,11 +10,19 @@ public class Player : MonoBehaviour
     private Camera camera;
     public float x_sensi;
     public float y_sensi;
+    private float moveSpeed = 10f;
 
+    private void Start()
+    {
+        this.UpdateAsObservable().Where(_ => Input.GetKey(KeyCode.W)).Subscribe(_ => UpMove());
+        this.UpdateAsObservable().Where(_ => Input.GetKey(KeyCode.S)).Subscribe(_ => DownMove());
+        this.UpdateAsObservable().Where(_ => Input.GetKey(KeyCode.A)).Subscribe(_ => LeftMove());
+        this.UpdateAsObservable().Where(_ => Input.GetKey(KeyCode.D)).Subscribe(_ => RightMove());
+    }
     // Update is called once per frame
     void Update()
     {
-        ViewpointChange();  
+        ViewpointChange();
     }
 
     private void ViewpointChange()
@@ -27,6 +37,29 @@ public class Player : MonoBehaviour
         camera.transform.Rotate(-y_Rotation, 0, 0);
     }
 
+    private void UpMove()
+    {
+        var dir = Quaternion.Euler(camera.transform.eulerAngles) * Vector3.forward;
+        transform.position += new Vector3(dir.x * Time.deltaTime, 0, dir.z * Time.deltaTime)*moveSpeed;
+    }
+
+    private void DownMove()
+    {
+        var dir = Quaternion.Euler(camera.transform.eulerAngles) * Vector3.back;
+        transform.position += new Vector3(dir.x * Time.deltaTime, 0, dir.z * Time.deltaTime)*moveSpeed;
+    }
+
+    private void LeftMove()
+    {
+        var dir = Quaternion.Euler(camera.transform.eulerAngles) * Vector3.left;
+        transform.position += new Vector3(dir.x * Time.deltaTime, 0, dir.z * Time.deltaTime)*moveSpeed;
+    }
+
+    private void RightMove()
+    {
+        var dir = Quaternion.Euler(camera.transform.eulerAngles) * Vector3.right;
+        transform.position += new Vector3(dir.x * Time.deltaTime, 0, dir.z * Time.deltaTime)*moveSpeed;
+    }
 }
 
 
